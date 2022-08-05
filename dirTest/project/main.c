6ae5c818e375c5384d6323c44306e1dd8688ff22
juanse <juanse.giraldom@gmail.com>
2022-08-05 14:21:09 -0500

    version 1

--- dirTest/project/main.c
+++ dirTest/project/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 
     if(fin == NULL){
         perror("fopen-fin fails: ");
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); 
     }
 
     char buffer[64];
